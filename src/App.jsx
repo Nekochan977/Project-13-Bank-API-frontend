6ba93031be731f0d9411bfed419caafb5470216c
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-03-22 15:56:23 +0100

    Navigation component base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from "./components/navigation/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <Navigation/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
