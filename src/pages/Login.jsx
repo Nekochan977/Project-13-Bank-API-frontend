404b3d1c9c4df0b003305a0b59836e1c99348bd3
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-03-23 15:12:14 +0100

    Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+
+import LoginModal from "../components/loginModal/LoginModal";
+const Login = () => {
+    return (
+        <main className="main bg-dark">
+            <LoginModal />
+        </main>
+    )
+}
+export default Login
