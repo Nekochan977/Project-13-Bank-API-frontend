b0627f649112395774b87fa31016bc5bbc891472
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-03-23 17:02:12 +0100

    creation user page

---
+++ src/pages/User.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/user/header/Header"
+import Account from "../components/user/account/Account"
+
+const User = () => {
+    return (
+        <main className="main bg-dark">
+            <Header />
+            <Account />
+        </main>
+    )
+}
+export default User
