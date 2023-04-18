6562047e93d56bc41ac23fa41aecb3f281ae3290
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-04-18 17:12:00 +0200

    add louOut function

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,8 +1,21 @@
 import Logo from "../../assets/argentBankLogo.png"
 import "./Navigation.css"
 import {Link} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+
+import {HOME} from "../../routes";
+import {LOGIN} from "../../routes";
+import {setToken, setUser} from "../../app/userSlice";
 
 const Navigation = () =>{
+    const user = useSelector((state)=>state.user.user)
+    const dispatch = useDispatch()
+    const logOut = (e) => {
+        dispatch(setUser(null));
+        dispatch(setToken(null))
+    }
+    console.log(user)
+
     return(
         <header>
             <nav className="main-nav">
@@ -15,10 +28,21 @@ const Navigation = () =>{
                     <h1 className="sr-only">Argent Bank</h1>
                 </Link>
                 <div>
-                    <Link className="main-nav-item" to="/login">
-                        <i className="fa fa-user-circle"></i>
-                        Sign In
-                    </Link>
+                    {
+                        user ? (user &&
+                                <Link className="main-nav-item" to={HOME} onClick={logOut}>
+                                    <i className="fa fa-sign-out"></i>
+                                    Sign Out
+                                </Link>
+                            )
+                            :
+                            (
+                                <Link className="main-nav-item" to={LOGIN}>
+                                    <i className="fa fa-user-circle"></i>
+                                    Sign In
+                                </Link>
+                            )
+                    }
                 </div>
             </nav>
         </header>
