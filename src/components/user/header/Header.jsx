bf5eb39995d290b23bc30e6b76aa1a31163c15b1
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-04-20 10:04:24 +0200

    toggle base

--- src/components/user/header/Header.jsx
+++ src/components/user/header/Header.jsx
@@ -1,13 +1,40 @@
 import "./Header.css"
 import { useSelector} from "react-redux"
+import {useState} from "react";
 
 const Header = () => {
-const user = useSelector(state => state.user.user)
-    return (
-        <div className="header">
-            {user && (<h1>Welcome back<br/>{user.firstName}!</h1>)}
-            <button className="edit-button">Edit Name</button>
-        </div>
-    )
+    const user = useSelector(state => state.user.user)
+    const [isEditMode, setIsEditMode] = useState(false)
+
+    if(isEditMode === false) {
+        return (
+            <div className="header">
+
+                {user && (<h1>Welcome back<br/>{user.firstName} {user.lastName}!</h1>)}
+                <button className="edit-button" onClick={() => setIsEditMode(!isEditMode)}>Edit Name</button>
+            </div>
+        )
+    } else {
+        return (
+            <div className="header">
+
+                <form>
+                    <label htmlFor="firstname"></label>
+                    <input
+                        type="text"
+                        id="firstname"
+                        placeholder={user.firstName}
+                    />
+                    <input
+                        type="text"
+                        id="lastname"
+                        placeholder={user.lastName}
+                    />
+                </form>
+                <button className="edit-button" onClick={() => setIsEditMode(!isEditMode)}>Edit Name</button>
+            </div>
+        )
+    }
+
 }
 export default Header
