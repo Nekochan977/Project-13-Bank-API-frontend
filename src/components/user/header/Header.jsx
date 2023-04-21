fa7d414c05e7853b4cad7a4b5eb2611ffa6b7722
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-04-21 16:28:30 +0200

    input onChange => changeName function

--- src/components/user/header/Header.jsx
+++ src/components/user/header/Header.jsx
@@ -4,7 +4,27 @@ import {useState} from "react";
 
 const Header = () => {
     const user = useSelector(state => state.user.user)
+    const token = useSelector((state)=>state.user.token)
     const [isEditMode, setIsEditMode] = useState(false)
+    console.log(user)
+    console.log(token)
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [hasFirstName, setHasFirstName] = useState(false)
+    const [hasLastName, setHasLastName] = useState(false)
+
+    const changeName = (e) => {
+        if(e.target.id === "firstname"){
+            setFirstName(e.target.value)
+            setHasFirstName(true)
+            console.log(firstName)
+        } else if (e.target.id === "lastname"){
+            setLastName(e.target.value)
+            setHasLastName(true)
+            console.log(lastName)
+        }
+    }
 
     if(isEditMode === false) {
         return (
@@ -24,13 +44,18 @@ const Header = () => {
                         type="text"
                         id="firstname"
                         placeholder={user.firstName}
+                        onChange={changeName}
                     />
+                    {hasFirstName === false ? <p>Veuillez entrer un prénom</p> : "" }
                     <input
                         type="text"
                         id="lastname"
                         placeholder={user.lastName}
+                        onChange={changeName}
                     />
+                    {hasLastName === false ? <p>Veuillez entrer un nom de famille</p> : "" }
                 </form>
+                <button className="edit-button" >Save Name</button>
                 <button className="edit-button" onClick={() => setIsEditMode(!isEditMode)}>Edit Name</button>
             </div>
         )
