e784d0c7017966294a70f0ade1f8c4959d806116
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-04-24 16:30:33 +0200

    step back on call api + condition to avoid access without authentication

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,12 +1,29 @@
 import Header from "../components/user/header/Header"
 import Account from "../components/user/account/Account"
-// import {useDispatch, useSelector} from "react-redux"
-// import {useEffect} from "react"
-// import {setUser} from "../app/userSlice"
-import {GetUser} from "../utils/api";
+import {useDispatch, useSelector} from "react-redux"
+import {useEffect} from "react"
+import {setUser} from "../app/userSlice"
+import {useNavigate} from "react-router-dom";
 
 const User = () => {
-GetUser()
+    const token = useSelector((state)=>state.user.token)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(token === null){
+            navigate("/login")
+        }
+
+        fetch('http://localhost:3001/api/v1/user/profile', {
+            method: "POST",
+            headers: {'Authorization': `Bearer ${token}`}
+        }).then(async (resp) => {
+            const user = await resp.json()
+            dispatch(setUser(user.body))
+        })
+    })
+
     return (
         <main className="main bg-dark">
             <Header />
