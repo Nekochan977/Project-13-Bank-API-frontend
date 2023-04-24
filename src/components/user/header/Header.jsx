fab314f8748950bf094cfa914dc2f4c4630694f3
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-04-24 15:12:10 +0200

    PUT call api

--- src/components/user/header/Header.jsx
+++ src/components/user/header/Header.jsx
@@ -1,28 +1,59 @@
 import "./Header.css"
-import { useSelector} from "react-redux"
-import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux"
+import {useEffect, useState} from "react";
+import {setToken, setUser} from "../../../app/userSlice";
 
 const Header = () => {
     const user = useSelector(state => state.user.user)
     const token = useSelector((state)=>state.user.token)
     const [isEditMode, setIsEditMode] = useState(false)
-    console.log(user)
-    console.log(token)
 
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
+    const [userFirstName, setUserFirstName] = useState("")
+    const [userLastName, setUserLastName] = useState("")
     const [hasFirstName, setHasFirstName] = useState(false)
     const [hasLastName, setHasLastName] = useState(false)
+    const dispatch = useDispatch()
 
     const changeName = (e) => {
         if(e.target.id === "firstname"){
-            setFirstName(e.target.value)
+            setUserFirstName(e.target.value)
             setHasFirstName(true)
-            console.log(firstName)
+
         } else if (e.target.id === "lastname"){
-            setLastName(e.target.value)
+            setUserLastName(e.target.value)
             setHasLastName(true)
-            console.log(lastName)
+
+        }
+    }
+
+    const saveName = async (e) =>{
+        e.preventDefault()
+        if(hasFirstName === true && hasLastName === true){
+            try {
+                let res = await fetch("http://localhost:3001/api/v1/user/profile", {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        firstName: userFirstName,
+                        lastName: userLastName,
+                    }),
+                })
+                let resJson = await res.json()
+                if(resJson.status === 200) {
+                    dispatch(
+                        setUser({
+                            ...user,
+                            firstName: userFirstName,
+                            lastName: userLastName
+                        }))
+                    setIsEditMode(false)
+                }
+            } catch (err) {
+                console.log(err);
+            }
         }
     }
 
@@ -55,8 +86,8 @@ const Header = () => {
                     />
                     {hasLastName === false ? <p>Veuillez entrer un nom de famille</p> : "" }
                 </form>
-                <button className="edit-button" >Save Name</button>
-                <button className="edit-button" onClick={() => setIsEditMode(!isEditMode)}>Edit Name</button>
+                <button className="edit-button" onClick={saveName}>Save Name</button>
+                <button className="edit-button" onClick={() => setIsEditMode(!isEditMode)}>Cancel Edit Name</button>
             </div>
         )
     }
